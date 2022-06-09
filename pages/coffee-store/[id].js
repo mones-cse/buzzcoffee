fcaf69ff57e896bb6582de955fad87a8934d87f9
mones <mones.cse@gmail.com>
2022-06-09 14:03:55 +0600

    implement getStaticProps and getStaticPaths with fallback false parameter

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import result from "../../data/coffee-stores.json";
+import Image from "next/image";
 
-const CoffeeStore = () => {
-  const router = useRouter();
-  const { id } = router.query;
+export async function getStaticProps({ params }) {
+  console.log({ params });
+  const store = result.find((eachStore) => eachStore.id == params.id);
+  console.log({ store });
+  return { props: { store } };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "0", val: "0" } },
+      { params: { id: "1", val: "1" } },
+    ],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = (props) => {
+  console.log({ props });
   return (
     <div>
       <Link href={"/"}>
@@ -12,7 +30,9 @@ const CoffeeStore = () => {
           <h2>Go back Home </h2>
         </a>
       </Link>
-      {id}
+      <h2>{props.store.name}</h2>
+      <p>{props.store.address}</p>
+      <Image src={props.store.imgUrl} width={500} height={400} />
     </div>
   );
 };
