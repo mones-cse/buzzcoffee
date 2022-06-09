24ab22a4b93ab26296a6e928fa6d25672b2c07be
mones <mones.cse@gmail.com>
2022-06-09 13:50:44 +0600

    added backlink

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,19 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
   const { id } = router.query;
-  return <div>{id}</div>;
+  return (
+    <div>
+      <Link href={"/"}>
+        <a>
+          <h2>Go back Home </h2>
+        </a>
+      </Link>
+      {id}
+    </div>
+  );
 };
 export default CoffeeStore;
