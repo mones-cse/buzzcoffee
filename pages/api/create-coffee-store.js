5fdd046570d8cd760a18c2a9045108f6aa727032
mones <mones.cse@gmail.com>
2022-06-14 01:28:32 +0600

    accept parameter from request body

--- pages/api/create-coffee-store.js
+++ pages/api/create-coffee-store.js
@@ -1,16 +1,15 @@
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID
-);
-
-const table = base("base-coffee-store");
+import table from "../../lib/aittable";
 
 export default async function createCoffeeStore(req, res) {
   if (req.method == "POST") {
     try {
+      const { id, name, address, neighbourhood, vote, imgUrl } = req.body;
+      if (!id) {
+        throw { message: "Id can't be empty" };
+      }
       const stores = await table
         .select({
-          filterByFormula: "id=0017",
+          filterByFormula: `id=${id}`,
           view: "Grid view",
         })
         .firstPage();
@@ -20,16 +19,17 @@ export default async function createCoffeeStore(req, res) {
         res.status(200).json(store);
       } else {
         //create
-        console.log("create store ok!!!");
+        if (!name || name.length < 1) {
+          throw { message: "Name can't be empty" };
+        }
         const createdStore = await table.create({
-          id: "0017",
-          name: "demo name",
-          address: "demo address",
-          neighbourhood: "demo neighbourhood",
-          vote: 100,
-          imgUrl: "https://picsum.photos/200",
+          id: `${id}`,
+          name,
+          address: address || "",
+          neighbourhood: neighbourhood || "",
+          vote: vote || 0,
+          imgUrl: imgUrl || "",
         });
-        console.log({ createdStore });
         res.status(200).json(createdStore.fields);
       }
     } catch (error) {
