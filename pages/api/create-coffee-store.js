32ddb41334725f48e1eec5cf2d333f96d209c00d
mones <mones.cse@gmail.com>
2022-06-13 21:36:14 +0600

    fetch store info from airtable

--- pages/api/create-coffee-store.js
+++ pages/api/create-coffee-store.js
@@ -4,11 +4,29 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 );
 
 const table = base("base-coffee-store");
-console.log({ table });
 
 export default async function createCoffeeStore(req, res) {
   if (req.method == "POST") {
-    res.status(200).json({ message: "CreteStore api" });
+    try {
+      const stores = await table
+        .select({
+          filterByFormula: "id=0017",
+          view: "Grid view",
+        })
+        .firstPage();
+      console.log("---", stores);
+      if (stores.length > 0) {
+        const store = stores[0].fields;
+        res.status(200).json(store);
+      } else {
+        console.log("create store ok!!!");
+        res
+          .status(200)
+          .json({ message: "No store with that id, create store please" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   } else {
     res
       .status(200)
